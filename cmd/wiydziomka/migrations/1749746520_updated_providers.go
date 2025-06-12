dc80e997c67561d7932cb7e3d1dcf61698608b0d
Piotr Skólski <piotr.skolski@wakacje.pl>
2025-06-12 19:21:16 +0200

    added default providers in migration

---
+++ cmd/wiydziomka/migrations/1749746520_updated_providers.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2249077391")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "bool2193750486",
+			"name": "isLocal",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2249077391")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool2193750486")
+
+		return app.Save(collection)
+	})
+}
